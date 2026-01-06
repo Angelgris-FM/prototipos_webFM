01686dd4470d3d306193fab9891ad9f9d2a2be89
+sev <vidal.sebastianm@gmail.com>
2026-01-06 16:13:54 -0300

    Update prototipo_3_web.py

--- prototipo_3_web.py
+++ prototipo_3_web.py
@@ -129,7 +129,7 @@ def temperatura_promedio(time):
     if st.button("Mostrar gráfico promedio"):
         grafico_promedio(df_temp, time)
 # --- Interfaz principal ---
-st.title("Simulación de evolución de  temperaturas  durante la fabricación del  acero")
+st.title("Simulación y evolución de  temperaturas  durante la fabricación del  acero")
 
 st.write("""El siguiente proyecto de caracter ingenieril tiene como objetivo
             analizar y visualizar la historia termica de una barra de metal( de 1 metro)
@@ -165,4 +165,5 @@ elif opcion == "Visualizar gráfica de superficie":
 if st.button("Mostrar mediciones de temperaturas(set de datos base)"):
 
     st.write(df_temp)
+
 
